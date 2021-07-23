8c9dcd8f819d891c79a881e03d74c0a637ffedd8
bazdg10 <bazdg10@gmail.com>
2021-07-23 19:43:01 +0530

    Db updated caching to be implemented

--- server/models/Room.js
+++ server/models/Room.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const roomSchema = Schema({
     name: { type: String, required:  true },
-    teams: { type: Array },
-    current: {type: Array, default: [0, 0]},
+    teams: { type: [String] },
+    moderator: {type: String, required: true},
+    current: {type: [Number], default: [0, 0]},
     game: { type: Number, default: 0 },
-    dummy: { type: Array },
+    dummy: { type: [String] },
     admin: {type: String},
-    score: {type: Array, default: [0, 0]},
+    score: {type: [Number], default: [0, 0]},
     rounds: { type: Number, default: 4},
-    words: {type: Array}
+    words: {type: [String]}
 })
 const Room = mongoose.model('room', roomSchema)
 module.exports = Room
