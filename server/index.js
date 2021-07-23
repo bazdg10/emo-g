0d529b2f0727427f674b5460d1f085af996a1c9c
bazdg10 <bazdg10@gmail.com>
2021-07-23 12:44:44 +0530

    Game features added

--- server/index.js
+++ server/index.js
@@ -7,17 +7,23 @@ const app = express()
 const bodyParser = require('body-parser')
 const { Room } = require('./models/Room.js')
 const { User } = require('./models/User.js')
+// app.use(cors())
+
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 app.use(bodyParser.json())
 const http = require('http')
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
+let io = require('socket.io')(server, {
   cors: {
     origin: '*',
   }
 })
+app.set('io', io)
+const router = express.Router()
+
+
 const PORT = 5000 || process.env.PORT
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => app.listen(PORT, () => console.log(`Server Started on ${PORT}`)) )
@@ -27,20 +33,88 @@ mongoose.connect(process.env.ROOM, { useNewUrlParser: true, useUnifiedTopology: 
         .then(()=> {console.log(`Rooms are active`)})
         .catch(error => console.log(error.message))
 
-app.post('/startRoom', (req, res) => {
+
+const emoMatrix = [
+  [ 'content', 'glad', 'happy', 'excited', 'ecstatic' ],
+  [ 'down', 'unhappy', 'sad', 'glum', 'depressed'],
+  [ 'upset', 'frustrated', 'angry', 'agitated', 'furious'],
+  [ 'aware', 'curious', 'interested', 'surprised', 'amazed' ],
+  [ 'startled', 'nervous', 'scared', 'afraid', 'terrified'],
+  [ 'drowsy', 'sleepy', 'tired', 'worn out', 'exhausted' ],
+  [ 'sorry', 'regretful', 'guilty', 'remorseful', 'ashamed'],
+  [ 'hopeful', 'sure', 'confident', 'strong', 'powerful'],
+  [ 'uneasy', 'tense', 'worried', 'anxious', 'panicked'],
+  [ 'capable', 'pleased', 'proud', 'dignified', 'triumphant']
+]
+
+app.post('/startRoom', async (req, res) => {
   var { name } = req.body
   name += new Date().toLocaleTimeString()
   var roomName = crypto.createHash('sha1').update(name).digest('hex')
   console.log(roomName)    
   const newRoom = new Room({roomName, dummy: [0]})
-  newRoom.save()
-  return res.status(200).send(roomName)
+  return res.status(200).send({ roomName: roomName, redirectUrl: `/admin/${generatedRoom._id}` })
 })
 
 io.on('connect', socket => {
+  
+  socket.on('moderator', ({room, word}) => {
+    console.log(room)
+    const r = Room.find({room})
+    if (r.game==1) {
+      words.push(word)
+      // Random allocation to a user
+      var player = Math.floor(Math.random()*10)
+      var user = User.find({tempId: r.teams[0][player].tempId}).tempId
+      io.sockets.in(user).emit('servergamemessage', word)
+      Room.findByIdAndUpdate(room._id, room, { new: true })
+    } else {
+      socket.emit('servermessage', { text: `WAIT FOR PLAYERS TO JOIN`})
+    }
+  })
+  
+  socket.on('gamemessage', ({room, word}) => {
+    console.log(room)
+    const r = Room.find({room})
+    if (r.game==1) {
+      if (r.words.length==1)
+      {
+      rooms.words.push(word)
+      r.rounds--;
+      var player = Math.floor(Math.random()*10)
+      var user = User.find({tempId: r.teams[1][player].tempId}).tempId
+      io.sockets.in(user).emit('servergamemessage', r.words[word.length-1])
+      Room.findByIdAndUpdate(room._id, room, { new: true })
+    } else {
+      rooms.words.push(word)
+      var origin = words[0]
+      // find distance of both words
+      var x;
+      for ( var i=0; i<emoMatrix.length; i++ ) {
+        x = emoMatrix[i].indexOf(origin)
+        if (x!=-1 ) {
+          origin = i
+          break    
+        }
+      }
+      var s = [0, 0]
+      for ( var i=0; i<2; i++ ) {
+        var j = emoMatrix[origin].indexOf(r.words[i+1])
+        if (j!=-1){
+          r.score[i] += (5-Math.abs(x-j))
+        } 
+      }      
+    Room.findByIdAndUpdate(room._id, r, {new: true})
+    socket.broadcast.to(r).emit('servergamemessage', { scores:`${r.score}` });
+    }
+  }
+  })
+  
+  
   socket.on('join', async ({room, email, name}, callback) => {
 
     var roomPool = await Room.find({room})
+    
     if (roomPool) {
         
       const userAlreadyJoined = User.find({name, email})
@@ -50,6 +124,7 @@ io.on('connect', socket => {
         socket.emit('servermessage', { text: `YOU ARE ALREADY PART OF A GAME`});
         uid = "DONT"
       }  
+      // Retrieve
       if (typeof(userAlreadyJoined)!="undefined")
         uid = userAlreadyJoined._id       
       if (uid!="DONT") {
@@ -69,18 +144,15 @@ io.on('connect', socket => {
         console.log('Guys joining the room')
         if ( prevRoom.game==1 ) {
           // Retrieve user's data and let him/ her join the room
+          
         }  
           prevRoom.dummy.push({email})
           if (prevRoom.dummy.length == 21) {
               console.log('All users have joined the game')
               // RANDOM ALLOTMENT FOR NOW
-              for ( var i=1; i<prevRoom.dummy.length; i++) {
-                  if (i%2==1) {
-                    prevRoom.team1.push(prevRoom.dummy[i])
-                  } else {
-                    prevRoom.team2.push(prevRoom.dummy[i])
-                  }
-              }
+              prevRoom.teams = [[], []]
+              for ( var i=1; i<prevRoom.dummy.length; i++) 
+                  prevRoom.team[i%2].push(prevRoom.dummy[i])  
             prevRoom.dummy.clear()
             prevRoom.game = 1
             } else {
@@ -116,12 +188,12 @@ io.on('connect', socket => {
         // User leaves before game starts        
           room.dummy.splice(idxOfLeave, 1)
         } else {
-          idxOfLeave = room.team1.indexOf(userLeaving.email)
+          idxOfLeave = room.teams[0].indexOf(userLeaving.email)
           if (idxOfLeave!=-1) {
-            room.team1.splice(idxOfLeave, 1)
+            room.teams[0].splice(idxOfLeave, 1)
           } else {
             idxOfLeave = room.team2.indexOf(userLeaving.email)
-            room.team2.splice(idxOfLeave, 1)
+            room.teams[1].splice(idxOfLeave, 1)
           }
         }
         User.findByIdAndUpdate(userLeaving._id, userLeaving, { new: true })
@@ -130,3 +202,10 @@ io.on('connect', socket => {
     }
   })
 })
+
+// const score = (emo) => {
+//     const idx = punchedIn[4]
+//     if (id)    
+// }
+
+
--- server/models/Room.js
+++ server/models/Room.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const roomSchema = Schema({
     name: { type: String, required:  true },
-    team1: { type: Array },
-    team2: { type: Array },
+    teams: { type: Array },
+    // team2: { type: Array },
     game: { type: Number, default: 0 },
-    dummy: { type: Array }
+    dummy: { type: Array },
+    admin: {type: String},
+    score: {type: Array, default: [0, 0]},
+    rounds: { type: Number, default: 4},
+    words: {type: Array}
 })
 const Room = mongoose.model('room', roomSchema)
 module.exports = Room
